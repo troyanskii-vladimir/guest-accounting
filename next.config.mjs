10bc97564575393a6ad9ee8fc5c540f615ce78f7
troyanskii-vladimir <118539771+troyanskii-vladimir@users.noreply.github.com>
2024-05-07 01:02:23 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = nextConfig = {
+  basePath: "/guest-accounting",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
